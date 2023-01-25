cac0606d6124e1211218bbcda0fad6c1bf61d37e
Ostap <43958957+pawas007@users.noreply.github.com>
2023-01-26 00:34:14 +0200

    Refactor home page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,21 +1,14 @@
 <template>
     <v-layout column align-center class="mt-10">
             <h1 class="display-1">Vue Laravel Application</h1>
-            <p>
-                Click user icon to SignUp or check users list
-            </p>
+            <p>Click user icon to SignUp or check users list</p>
             <ul>
-                <li>
-                    Check your env
-                </li>
-                <li>
-                    Run migrate and seed
-                </li>
-                <li>
-                    Run queue
-                </li>
+                <li>Check your env</li>
+                <li>Run migrate and seed</li>
+                <li>Run queue</li>
                 <li>SMS service - https://www.twilio.com</li>
                 <li>STMP - https://mailtrap.io</li>
+                <li>!All services on test mode</li>
             </ul>
         </v-layout>
 </template>
